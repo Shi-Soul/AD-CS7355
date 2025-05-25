79031d3a3ef382d3cd53737901093366cf39dab0
Shi-Soul <993520026@qq.com>
2025-05-26 00:35:50 +0800

    .

--- CARLA/project3/run.py
+++ CARLA/project3/run.py
@@ -7,6 +7,7 @@ def run_task(task_index):
 
 def run_all_tasks():
     for task_index in range(11):  # Range from 0 to 10
+        print("Starting task index:", task_index)
         run_task(task_index)
 
 if __name__ == "__main__":
@@ -16,7 +17,6 @@ if __name__ == "__main__":
 
     if args.task_index is not None:
         if 0 <= args.task_index <= 10:
-            print("Starting task index:", args.task_index)
             run_task(args.task_index)
         else:
             print("Invalid task index. Please provide a value between 0 and 10.")
